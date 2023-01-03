06d4d5e8b6a8a6b86b8dc9c8be13e0dd2731cce8
IhorDiu <110743288+IhorDiu@users.noreply.github.com>
2023-01-03 04:03:46 +0200

    Task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+
+startBtn.addEventListener('click', startBgColorChange);
+
+let intervalId = null;
+stopBtn.disabled = true;
+
+function startBgColorChange() {
+    
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    stopBtn.addEventListener('click', stopBgColorChange);
+    
+    intervalId = setInterval(() => changeBgColor(), 1000);
+  };
+
+function stopBgColorChange () {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+};
+
+function changeBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
