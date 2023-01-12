5f64d0ae8f9d3556239919a503427ce1a7db71e2
IhorDiu <110743288+IhorDiu@users.noreply.github.com>
2023-01-12 23:36:06 +0200

    Task_2 Finaly

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,33 +1,78 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  timerFace: document.querySelector('#datetime-picker'),
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMinutes: document.querySelector('[data-minutes]'),
+  dataSeconds: document.querySelector('[data-seconds]'),
+};
 
-ref {
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  timer.start();
+});
 
-}
+const timer = {
+  start() {
+    let intervalId = setInterval(() => {
+      const futureDate = new Date(refs.timerFace.value);
+      const currentTime = Date.now();
+      const deltaTime = futureDate - currentTime;
+      const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+      if (deltaTime >= 0) {
+        showTimer({ days, hours, minutes, seconds });
+      } else {
+        Notiflix.Notify.success('The time is up!!!');
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  },
+};
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      refs.startBtn.disabled = false;
+    }
+  },
+};
 
-flatpickr(selector, options);
+flatpickr(refs.timerFace, options);
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, 0);
+}
+function showTimer({ days, hours, minutes, seconds }) {
+  refs.dataDays.textContent = days;
+  refs.dataHours.textContent = hours;
+  refs.dataMinutes.textContent = minutes;
+  refs.dataSeconds.textContent = seconds;
+}
